9c1c07760f2781e92af980d9e95254ff75ffbf5c
clearghost3 <165910897+clearghost3@users.noreply.github.com>
2024-08-03 04:04:19 +0900

    update:add RoleGuard

--- src/modules/trades/trades.controller.ts
+++ src/modules/trades/trades.controller.ts
@@ -55,27 +55,29 @@ import {
   ApiDefaultResponse,
 } from '@nestjs/swagger';
 import { Role } from 'src/commons/types/users/user-role.type';
+import { RolesGuard } from '../auth/utils/roles.guard';
+import { Roles } from '../auth/utils/roles.decorator';
 
 @ApiTags(SWAGGER.TRADES.TRADES_API_TAGS)
-@ApiBearerAuth()
 @Controller('trades')
 export class TradesController {
   constructor(private readonly tradesService: TradesService) {}
 
   //테스트 메서드==============================
-
+  @ApiBearerAuth()
   @Get('/hello')
   @ApiOperation({
     summary: SWAGGER.TRADES.HELLO.API_OPERATION.SUMMARY,
     description: SWAGGER.TRADES.HELLO.API_OPERATION.DESCRIPTION,
   })
   @ApiOkResponse({ description: 'hello를 출력합니다' })
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles(Role.USER)
   async hello(@Req() req: { user: User }) {
     const user = req.user;
     return await this.tradesService.hello(user.id);
   }
-
+  @ApiBearerAuth()
   @Get('/test')
   @ApiOperation({
     summary: SWAGGER.TRADES.TEST.API_OPERATION.SUMMARY,
@@ -84,6 +86,8 @@ export class TradesController {
   @ApiOkResponse({
     description: '실행이 완료되면 bbbbbbbbbbbbbbbbbbbbbbbb를 출력합니다',
   })
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles(Role.USER)
   async test(@Body() testDto: TestDto) {
     console.log('AAAAAAAAAAAAAAAAA');
     return await this.tradesService.test();
@@ -95,7 +99,8 @@ export class TradesController {
     description: SWAGGER.TRADES.CHANGE_ROLE.API_OPERATION.DESCRIPTION,
   })
   @ApiNoContentResponse({ description: '유저의 계정이 변경됩니다' })
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles(Role.USER)
   async changeRole(@Req() req: { user: User }) {
     const user = req.user;
     return await this.tradesService.changeRole(user.id);
@@ -106,25 +111,30 @@ export class TradesController {
     summary: SWAGGER.TRADES.CHANGE_REMAIN_SEAT.API_OPERATION.SUMMARY,
     description: SWAGGER.TRADES.CHANGE_REMAIN_SEAT.API_OPERATION.DESCRIPTION,
   })
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles(Role.USER)
   async changRemainSeat(@Param('scheduleId', ParseIntPipe) scheduleId: number) {
     return await this.tradesService.changRemainSeat(scheduleId);
   }
 
   //테스트 메서드========================================================
 
-  //중고 거래 로그 종회
+  //<1>중고 거래 로그 종회
+  @ApiBearerAuth()
   @Get('tradelogs')
   @ApiOperation({
     summary: SWAGGER.TRADES.GET_TRADE_LOGS.API_OPERATION.SUMMARY,
     description: SWAGGER.TRADES.GET_TRADE_LOGS.API_OPERATION.DESCRIPTION,
   })
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles(Role.USER)
   async getLogs(@Req() req: { user: User }) {
     const user = req.user;
     return await this.tradesService.getLogs(user.id);
   }
 
-  //중고 거래 목록 조회
+  //<2>중고 거래 목록 조회
+  @ApiBearerAuth()
   @Get()
   @ApiOperation({
     summary: SWAGGER.TRADES.GET_TRADE_LIST.API_OPERATION.SUMMARY,
@@ -134,19 +144,22 @@ export class TradesController {
     return await this.tradesService.getList();
   }
 
-  //중고 거래 생성
+  //<3>중고 거래 생성
+  @ApiBearerAuth()
   @Post()
   @ApiOperation({
     summary: SWAGGER.TRADES.CREATE_TRADE.API_OPERATION.SUMMARY,
     description: SWAGGER.TRADES.CREATE_TRADE.API_OPERATION.DESCRIPTION,
   })
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles(Role.USER)
   async createTrade(@Body() createTradeDto: CreateTradeDto, @Req() req: { user: User }) {
     const user = req.user;
     return await this.tradesService.createTrade(createTradeDto, user.id);
   }
 
-  //중고 거래 상세 조회
+  //<4>중고 거래 상세 조회
+  @ApiBearerAuth()
   @Get('/:tradeId')
   @ApiOperation({
     summary: SWAGGER.TRADES.GET_DETAILED_TRADE.API_OPERATION.SUMMARY,
@@ -157,13 +170,15 @@ export class TradesController {
   }
   //첫 주솟값을 param으로 받는 콘트롤러 메서드
 
-  //중고 거래 수정
+  //<5>중고 거래 수정
+  @ApiBearerAuth()
   @Patch('/:tradeId')
   @ApiOperation({
     summary: SWAGGER.TRADES.UPDATE_TRADE.API_OPERATION.SUMMARY,
     description: SWAGGER.TRADES.UPDATE_TRADE.API_OPERATION.DESCRIPTION,
   })
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles(Role.USER)
   async updateTrade(
     @Param('tradeId', ParseIntPipe) tradeId: number,
     @Body() updateTradeDto: UpdateTradeDto,
@@ -173,26 +188,30 @@ export class TradesController {
     return await this.tradesService.updateTrade(tradeId, updateTradeDto, user.id);
   }
 
-  //중고 거래 삭제
+  //<6>중고 거래 삭제
+  @ApiBearerAuth()
   @Delete('/:tradeId')
   @ApiOperation({
     summary: SWAGGER.TRADES.DELETE_TRADE.API_OPERATION.SUMMARY,
     description: SWAGGER.TRADES.DELETE_TRADE.API_OPERATION.DESCRIPTION,
   })
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles(Role.USER)
   async deleteTrade(@Param('tradeId', ParseIntPipe) tradeId, @Req() req: { user: User }) {
     const user = req.user;
     await this.tradesService.deleteTrade(tradeId, user.id);
     return { message: MESSAGES.TRADES.SUCCESSFULLY_DELETE.TRADE };
   }
 
-  //중고 거래 구매
+  //<7>중고 거래 구매
+  @ApiBearerAuth()
   @Post('/:tradeId')
   @ApiOperation({
     summary: SWAGGER.TRADES.PURCHASE_TICKET.API_OPERATION.SUMMARY,
     description: SWAGGER.TRADES.PURCHASE_TICKET.API_OPERATION.DESCRIPTION,
   })
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles(Role.USER)
   async createTicket(@Param('tradeId', ParseIntPipe) tradeId, @Req() req: { user: User }) {
     const user = req.user;
     return await this.tradesService.createTicket(tradeId, user.id);
