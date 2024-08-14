8975bbadbeeb8d1a6762f84e5dc4d7bfad1578ba
komiharuu <biomilky3@gmail.com>
2024-08-14 14:00:14 +0900

    Update/ createShowReview api

--- src/modules/show-reviews/show-reviews.controller.ts
+++ src/modules/show-reviews/show-reviews.controller.ts
@@ -1,34 +1,71 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { ShowreviewsService } from './show-reviews.service';
-import { CreateShowreviewDto } from './dto/create-showreview.dto';
-import { UpdateShowreviewDto } from './dto/update-showreview.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Req,
+  UseGuards,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import { ShowReviewsService } from './show-reviews.service';
+import { CreateShowReviewDto } from './dto/create-show-review.dto';
+import { UpdateShowReviewDto } from './dto/update-show-review.dto';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Roles } from '../auth/utils/roles.decorator';
+import { RolesGuard } from '../auth/utils/roles.guard';
+import { Role } from 'src/commons/types/users/user-role.type';
 
-@Controller('/shows/:showId/reviews')
-export class ShowreviewsController {
-  constructor(private readonly showreviewsService: ShowreviewsService) {}
+@ApiTags('공연 리뷰')
+@Controller('shows/reviews')
+export class ShowReviewsController {
+  constructor(private readonly showReviewsService: ShowReviewsService) {}
 
-  @Post()
-  create(@Body() createShowreviewDto: CreateShowreviewDto) {
-    return this.showreviewsService.create(createShowreviewDto);
+  /**
+   * 공연 리뷰 작성
+   * @param showId
+   * @param createShowReviewDto
+   * @param req
+   * @returns
+   * */
+  @ApiBearerAuth()
+  @Roles(Role.USER)
+  @UseGuards(RolesGuard)
+  @HttpCode(HttpStatus.CREATED)
+  @Post('/:showId')
+  async createShowReview(
+    @Param('showId') showId: number,
+    @Body() createShowreviewDto: CreateShowReviewDto,
+    @Req() req: any
+  ) {
+    const showReview = await this.showReviewsService.createShowReview(
+      createShowreviewDto,
+      showId,
+      req.user
+    );
+    return { status: HttpStatus.CREATED, message: '리뷰 작성에 성공했습니다', data: showReview };
   }
 
   @Get()
   findAll() {
-    return this.showreviewsService.findAll();
+    return this.showReviewsService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.showreviewsService.findOne(+id);
+    return this.showReviewsService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateShowreviewDto: UpdateShowreviewDto) {
-    return this.showreviewsService.update(+id, updateShowreviewDto);
+  update(@Param('id') id: string, @Body() updateShowreviewDto: UpdateShowReviewDto) {
+    return this.showReviewsService.update(+id, updateShowreviewDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.showreviewsService.remove(+id);
+    return this.showReviewsService.remove(+id);
   }
 }
