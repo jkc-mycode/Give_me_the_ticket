dceb68e961a564ebe07d858f6bbad87572b5a0e3
clearghost3 <165910897+clearghost3@users.noreply.github.com>
2024-08-21 17:57:13 +0900

    update:redirect back history

--- src/public/js/trades/trades-detail.js
+++ src/public/js/trades/trades-detail.js
@@ -77,8 +77,10 @@ document.addEventListener('DOMContentLoaded', async () => {
           }
         );
         alert('거래에 성공했습니다!');
+        window.location.href = '/views/users/me#trade';
+      } else {
+        window.history.back();
       }
-      window.location.href = '/views/users/me#trade';
     } catch (err) {
       console.error('중고 거래 구매에 실패했습니다.', err);
       alert('중고 거래 구매에 실패했습니다.');
